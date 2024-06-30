9e58e43211c6740e9c84e77d87370d071b28479c
sho5081857 <masashi713416@gmail.com>
2024-06-30 13:43:19 +0900

    first commit

---
+++ src/bin/main.rs
@@ -0,0 +1,8 @@
+use loco_rs::cli;
+use next_learn_api_rust_loco::app::App;
+use migration::Migrator;
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    cli::main::<App, Migrator>().await
+}
